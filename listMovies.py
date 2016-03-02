ae5b7d514e216b4d37c7352b75163392e6d44ae4
Akshay Pratap Singh <pratapakshay0@gmail.com>
2016-03-03 01:22:37 +0530

    (fixed): added new functions to improve structure

--- listMovies.py
+++ listMovies.py
@@ -16,36 +16,55 @@ import traceback
 import pprint
 import datetime
 import codecs
+import shutil
 
 from jinja2 import FileSystemLoader
 from jinja2 import Environment
 
+# Full absolute path of template directory
+TEMPLATES_DIR = os.path.dirname(os.path.abspath(__file__)) + "/templates"
+
+# Full absolute path of output directory
+OUTPUT_DIR = os.path.dirname(os.path.abspath(__file__)) + "/content"
+
+
 def datetimeformat(value, format='%I:%M %p , %d-%m-%Y'):
+    """
+    A Jinja template filter to print date object in human
+    readable date string
+    Ex: datetime.datetime(2016, 3, 3, 0, 40, 40, 921159)
+        ==> 12:40 AM , 03-03-2016
+    """
     return value.strftime(format)
 
-def create_output_dir_if_not_exists():
-    curr_dir = os.path.dirname(os.path.abspath(__file__))
-    print curr_dir
-    if not os.path.exists(curr_dir+"/content"):
-        os.makedirs(curr_dir+"/content")
+
+def clean_and_create_output_dir():
+    """
+    Deletes a output directory if exists, and then create a
+    fresh output directory
+    """
+    shutil.rmtree(OUTPUT_DIR,  ignore_errors=True)
+    os.makedirs(OUTPUT_DIR)
+
 
 def createHtml(movies_listing):
-    '''
-    To create index HTML page
-    '''
-    env = Environment(loader=FileSystemLoader('templates'))
+    """
+    Creates a index.html page with data
+    """
+    env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
     env.filters['datetimeformat'] = datetimeformat
     template = env.get_template('index-template.html')
     output_template = template.render(movies_listing=movies_listing)
 
-    with codecs.open("content/index.html", "w+", "utf-8") as fh:
+    with codecs.open(OUTPUT_DIR + "/index.html", "w+", "utf-8") as fh:
         fh.write(output_template)
 
+
 def createMarkdown(movies_listing):
-    '''
-    To create index Markdown page
-    '''
-    with codecs.open("content/index.md", "w+", "utf-8") as fh:
+    """
+    Creates a index.md page with data
+    """
+    with codecs.open(OUTPUT_DIR + "/index.md", "w+", "utf-8") as fh:
         for d in movies_listing:
             dirname = d["dirname"]
             movies = d["movies"]
@@ -54,13 +73,65 @@ def createMarkdown(movies_listing):
                 fh.write("%d. %s\n"%(movies.index(movie)+1, movie))
             fh.write("\n")
 
+
+def copy(src, dest):
+    """
+    Copies both files or directories from source to destination
+    """
+    try:
+        shutil.copytree(src, dest, ignore=shutil.ignore_patterns(
+            '*.py', '*.sh'))
+    except OSError as e:
+        # If the error was caused because the source wasn't a directory
+        # i.e it's a file
+        if e.errno == shutil.errno.ENOTDIR:
+            shutil.copy(src, dest)
+        else:
+            print src, dest
+            print('Directory not copied. Error: %s' % e)
+
+
+def copy_static_folder():
+    """
+    Copies a static folder to output directory
+    """
+    copy(TEMPLATES_DIR + "/static", OUTPUT_DIR+"/static")
+
+
+def copy_CNAME_file():
+    """
+    Copies a CNAME file to output directory
+    """
+    copy(TEMPLATES_DIR + "/CNAME", OUTPUT_DIR)
+
+
+def create_output(movies_listing):
+    """
+    Runs all actions required to create an output for deployment
+    """
+    # Create output directory
+    clean_and_create_output_dir()
+
+    # Create a markdown file
+    createMarkdown(movies_listing)
+
+    # Create a html file
+    createHtml(movies_listing)
+
+    # Copy static folder
+    copy_static_folder()
+
+    # Copy CNAME file
+    copy_CNAME_file()
+
+
 def main():
     # a output list of dicts in which each dict contains data of one
     # particular directory
     movies_listing = []
     try :
         # get environment variable value
-        movies_path = os.getenv('MOVIES_FOLDER_PATH')
+        movies_path = os.environ.get("MOVIES_FOLDER_PATH", None)
 
         # check if environment variable exists or not
         if os.path.exists(movies_path):
@@ -88,10 +159,8 @@ def main():
                         "movies"  : movies
                         })
 
-            # pprint.pprint(movies_listing)
-            create_output_dir_if_not_exists()
-            createMarkdown(movies_listing)
-            createHtml(movies_listing)
+            # create an output for deployment
+            create_output(movies_listing)
 
         else:
             print "Error :: No MOVIES_FOLDER_PATH environment variable set."
